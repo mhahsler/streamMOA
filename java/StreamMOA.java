bb4f05761f5b70709ea5c43fb1c449df6dd8720f
mhahsler <michael@hahsler.net>
2018-01-12 08:49:55 -0600

    Removed unnecessary imports.

--- java/StreamMOA.java
+++ java/StreamMOA.java
@@ -1,14 +1,7 @@
 import java.util.ArrayList;
 
-import moa.cluster.Cluster;
-import moa.cluster.Clustering;
 import moa.clusterers.AbstractClusterer;
-import moa.clusterers.macro.dbscan.DBScan;
-import moa.core.Measurement;
-import moa.options.FloatOption;
-import moa.options.IntOption;
 import weka.core.DenseInstance;
-import weka.core.Instance;
 
 class StreamMOA {
   public static void update(AbstractClusterer clusterer, double[][] points) {
